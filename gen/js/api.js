176b6b1414b941f80dfa4f42257cae0d40daccc6
i18n <i18n.site@gmail.com>
2025-02-07 15:10:38 +0800

    ♨

--- gen/js/api.js
+++ gen/js/api.js
@@ -1,9 +1,8 @@
 import {
-  T3Encode,
-  T2Decode,T4Decode,
-  T0Encode, // CallEncode,
-  T1Encode, //CallLiEncode,
-  T1Decode // BinLiDecode
+  T2Encode,
+  T1Decode,T3Decode,
+  T0Encode, //CallLiEncode,
+  T0Decode // BinLiDecode
 } from './_.pb.js'
 
 import hpc from 'u/hpc.js'
@@ -12,11 +11,11 @@ const [
   _set,
   _noArgs,
   _req
-] = hpc(T0Encode, T1Encode, T1Decode)
+] = hpc(T0Encode, T0Decode)
 
 export const set = _set;
 
-export const captcha = _noArgs(1,T2Decode) /* id:Box<[u8]>,img:Box<[u8]>,tip:Box<[u8]> */
-export const authSigninMail = (address /* str */,password /* str */)=>_req(2,T3Encode([address,password]))
-export const authSignupMail = (address /* str */,password /* str */)=>_req(3,T3Encode([address,password]))
-export const authSignupMailVerify = (address /* str */,code /* str */)=>_req(4,T4Decode,T3Encode([address,code])) /* SIGNUP_MAIL_VERIFY:enum */
+export const captcha = _noArgs(1,T1Decode) /* id:Box<[u8]>,img:Box<[u8]>,tip:Box<[u8]> */
+export const authSigninMail = (address /* str */,password /* str */)=>_req(2,T2Encode([address,password]))
+export const authSignupMail = (address /* str */,password /* str */)=>_req(3,T2Encode([address,password]))
+export const authSignupMailVerify = (address /* str */,code /* str */)=>_req(4,T3Decode,T2Encode([address,code])) /* SIGNUP_MAIL_VERIFY:enum */
